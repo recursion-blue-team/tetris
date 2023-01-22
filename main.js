6f5394029ea683f79094584e7ee03f3d077b8239
eddie-eights <fuk8eightuda@gmail.com>
2023-01-23 00:43:02 +0900

    feat: テトロミノの移動 #7

--- main.js
+++ main.js
@@ -2,10 +2,21 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+
 //ブロック１単位のピクセルサイズ
 const BLOCK_SIZE = 30;
 //テトロミノのサイズ
 const TETRO_SIZE = 4;
+
+//フィールドサイズ(縦、横)
+const FIELD_COL = 10;
+const FIELD_ROW = 20;
+
+//キャンバスサイズ
+const SCREEN_WIDTH = BLOCK_SIZE * FIELD_COL; // 300px
+const SCREEN_HEIGHT = BLOCK_SIZE * FIELD_ROW; // 600px
+canvas.width = SCREEN_WIDTH;
+canvas.height = SCREEN_HEIGHT;
 
 
 const TETRO_TYPES = [
@@ -70,22 +81,60 @@ const TETRO_TYPES = [
     ]
 ];
 
+
+// テトロミノの初期座標
+let tetro_x = 0;
+let tetro_y = 0;
+
 // 描画対象のテトロミノ
 let tetro = TETRO_TYPES[3];
 
-// canvasに描画
-for(let y = 0; y < TETRO_SIZE; y++)
-{
-    for(let x = 0; x < TETRO_SIZE; x++)
+
+draw_tetro();
+
+//テトロミノを描画する関数です
+function draw_tetro()
+{   
+    context.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    for(let y = 0; y < TETRO_SIZE; y++)
     {
-        if(tetro[x][y])
+        for(let x = 0; x < TETRO_SIZE; x++)
         {
-            let px = x * BLOCK_SIZE;
-            let py = y * BLOCK_SIZE;
-            context.fillStyle = "red";
-            context.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
-            context.strokeStyle = "black";
-            context.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+            if(tetro[y][x])
+            {
+                let px = (tetro_x + x) * BLOCK_SIZE;
+                let py = (tetro_y + y) * BLOCK_SIZE;
+                context.fillStyle = "red";
+                context.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+                context.strokeStyle = "black";
+                context.strokeRect(px, py, BLOCK_SIZE, BLOCK_SIZE);
+            }
         }
     }
 }
+
+
+// テトロミノの移動するイベント関数です。
+document.onkeydown = function(e)
+{    
+    switch(e.key)
+    { 
+        case "ArrowLeft": // ←
+            tetro_x--;
+            break;
+        case "ArrowUp": // ↑
+            tetro_y--;
+            break;
+        case "ArrowRight": // →
+            tetro_x++;
+            break;
+        case "ArrowDown": // ↓
+            tetro_y++;
+            break;
+        case "Space": // スペースキー(回転)
+            //回転する処理が入ります。
+            break;
+    }
+    draw_tetro();
+} 
